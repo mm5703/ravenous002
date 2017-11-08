1aafd2c59b2d2f07a07605d4a0c557864e8697bc
MM5703 <Master@Amals-MacBook-Pro-175.local>
2017-11-08 23:09:18 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'; //import React and its Component
+import logo from './logo.svg'; //import logo from current dir
+import './App.css';//import css style
+import BusinessList from './components/BusinessList/BusinessList.js';   //import BussinesList instance
+import SearchBar from './components/SearchBar/SearchBar.js';//import SearchBar instance
+
+let business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+let businesses =[business, business, business, business, business, business];
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+  <h1>ravenous</h1>
+  <SearchBar />
+  <BusinessList businesses={businesses} />
+</div>
+    );
+  }
+}
+
+export default App;
